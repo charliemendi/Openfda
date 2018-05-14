d82873bdb390134af095dd7b84a493dc3b70472c
cmendi <carlos.mendivilg@gmail.com>
2018-05-14 14:48:02 +0200

    Practica 1 Mejorado

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -6,25 +6,38 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def Sin_parametros():
-    archivo ='''<form action="/my-handling-form-page" method = "post">
-                <div>
-                    <label for ="name" > Name:</label>
-                    <input type = "text" id = "name"/>
-                </div>
+    archivo ="""<!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <title>OpenFDA-project</title>
+            </head>
+            <body>
+    
+            <form action = "/listDrugs" method="get">
+              <input type="submit" value="Listar fármacos">
+                Limite: <input type="text" name="limit" value="1">
+            </form>
+    
+            <form action = "/manufacturer_name" method="get">
+              <input type="submit" value="Listar empresas">
+            </form>
+    
+            <form action = "/SearchDrug" method="get">
+              <input type="submit" value="Buscar fármaco">
+                Campo: <input type="text" name="Principio activo" value="">
+            </form>
+    
+            <form action = "/SearchCompany" method="get">
+              <input type="submit" value="Buscar empresas">
+                Campo: <input type="text" name="Nombre empresaa" value="">
+            </form>
 
-                <div>
-                    <label for ="limite" > limit:</label>
-                    <input type = "limite" id = "limit"/>
-                </div>
-                
-                <divclass ="button">
-                    <button type = "submit" > Send your message </button>
-                </div>
-            </form>'''
+            </body>
+            </html>"""
     return archivo
 @app.route("/listDrugs")
 def listDrugs():
-    number=int(input("Introduzca un numero del 1 al 100:"))
     headers = {'User-Agent': 'http-client'}
     conn = http.client.HTTPSConnection("api.fda.gov")
     conn.request("GET", "/drug/label.json?limit=10", None, headers)
@@ -50,7 +63,66 @@ def listDrugs():
                                     </body >
                                 </html >""" % (List1)
     return contenido
+@app.route("/SearchDrug")
+def listDrugs():
+    headers = {'User-Agent': 'http-client'}
+    conn = http.client.HTTPSConnection("api.fda.gov")
+    conn.request("GET", "/drug/label.json?limit=10", None, headers)
+    r1 = conn.getresponse()
+    print(r1.status, r1.reason)
+    repos_raw = r1.read().decode("utf-8")
+    conn.close()
 
+    repos = json.loads(repos_raw)
+    n = 0
+    List1 = []
+    while n != 10:
+        try:
+            if value == repos["results"][n]["activate_ingredient"]:
+                List1.append(repos["results"][n]["openfda"]["generic_name"])
+            else:
+                continue
+        except:
+            print("desconocido")
+        n += 1
+    contenido = """< !doctypehtml >
+                                <html>
+                                    <body style = 'background-color: blue' >
+                                        <h1> Medicamentos </h2>
+                                        <p> Generic_name: %s </p>
+                                    </body >
+                                </html >""" % (List1)
+    return contenido
+@app.route("/SearchCompany")
+def listDrugs():
+    headers = {'User-Agent': 'http-client'}
+    conn = http.client.HTTPSConnection("api.fda.gov")
+    conn.request("GET", "/drug/label.json?limit=10", None, headers)
+    r1 = conn.getresponse()
+    print(r1.status, r1.reason)
+    repos_raw = r1.read().decode("utf-8")
+    conn.close()
+
+    repos = json.loads(repos_raw)
+    n = 0
+    List1 = []
+    while n != 10:
+        try:
+            if value == repos["results"][n]["openfda"]["manufacturer_name"]:
+                List1.append(repos["results"][n]["openfda"]["manufacturer_name"])
+            else:
+                continue
+        except:
+            print("desconocido")
+        n += 1
+    contenido = """< !doctypehtml >
+                                <html>
+                                    <body style = 'background-color: blue' >
+                                        <h1> Medicamentos </h2>
+                                        <p> Generic_name: %s </p>
+                                    </body >
+                                </html >""" % (List1)
+    return contenido
 @app.route("/manufacturer_name")
 def manufacturer_name():
     headers = {'User-Agent': 'http-client'}
@@ -80,4 +152,4 @@ def manufacturer_name():
     return contenido2
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5329)
+    app.run(host='0.0.0.0', port=5399)
